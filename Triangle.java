44b1de451f6c43a71086ca84a0a8c4ca124625a4
khojothuglyf <khojothuglyf@icloud.com>
2026-06-12 07:03:57 +0000

    Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,15 @@
+public class  Triangle {
+
+    public static void main(String[] args) {
+
+        System.out.print("             T         ");
+        System.out.print("            TTT        ");
+        System.out.print("           TTTTT       ");
+        System.out.print("          TTTTTTT      ");
+        System.out.print("         TTTTTTTTT     ");
+        System.out.print("       TTTTTTTTTTTTT   ");
+        System.out.print("      TTTTTTTTTTTTTTT  ");
+
+    }
+
+}
