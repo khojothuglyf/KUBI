e1c8a04b5a4694da87e010456355c1782a9d379d
khojothuglyf <khojothuglyf@icloud.com>
2026-06-12 07:03:04 +0000

    TableAndChairs.java

---
+++ TableAndChairs.java
@@ -0,0 +1,15 @@
+public class TableAndChairs {
+
+    public static void main(String[] args) {
+
+        System.out.print("x                                  x");
+        System.out.print("x                                  x");
+        System.out.print("x                                  x");
+        System.out.print("xxxxx            xxxxxxxx      xxxxx");
+        System.out.print("x    x           x      x          x");
+        System.out.print("x    x           x      x          x");
+        System.out.print("x    x           x      x          x");
+        
+    }
+
+}
